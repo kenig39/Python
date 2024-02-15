24b8d1ebfa6ad8382ed27455a5498205c66bf918
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-15 22:43:51 +0300

    add fire

--- main.py
+++ main.py
@@ -77,6 +77,37 @@ class Enemy(Sprite):
         self.speed = 6
         self.setheading(random.randint(0,360))
 
+
+
+class Missile(Sprite):
+    def __init__(self, spiteshape, color, startx, starty ):
+        Sprite.__init__(self,spiteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.speed = 20
+        self.status = "ready"
+        self.goto(-1000, 1000)
+
+    def fire(self):
+        if self.status == "ready":
+            self.goto(player.xcor(),player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+
+    def move(self):
+        if self.status == "ready":
+            self.goto(-1000, 1000)
+
+        if self.status == "firing":
+            self.fd(self.speed)
+
+# Border check
+        if self.xcor() < -290 or self.xcor() > 290 or \
+            self.ycor()< -290 or self.ycor()> 290:
+            self.goto(-1000, 1000)
+            self.status = "ready"
+
+
+
 class Game:
     def __init__(self):
         self.level = 1
@@ -108,17 +139,22 @@ game.draw_border()
 #Create my spites
 player = Player("triangle", "white", 0, 0)
 enemy = Enemy("circle", "red", -100, 0)
+missile = Missile("triangle", "yellow", 0, 0)
+
 
 #Keyboard bindings
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
+
 turtle.listen()
 # main game loop
 while True:
     player.move()
     enemy.move()
+    missile.move()
 
 # check is collision
     if player.is_collision(enemy):
@@ -126,3 +162,8 @@ while True:
         y = random.randint(-250, 250)
         enemy.goto(x,y)
 
+    if missile.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
+        missile.status = "ready"
